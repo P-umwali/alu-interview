fea2514699a98230b8f7ebf0562f427eb2259805
Pamela UMWALI <p.umwali@alustudent.com>
2023-06-29 18:24:29 +0200

    new

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
+"""
+    0-pascal_triangle.py
+"""
+
+
 def pascal_triangle(n):
     """
-    Generate Pascal's triangle up to level n.
+        Return: Pascal’s triangle of n
     """
-
     if n <= 0:
         return []
 
     triangle = [[1]]
-
     for i in range(1, n):
         row = [1]
-        prev_row = triangle[i-1]
-        
-        """list: A list of lists representing Pascal's triangle up to level n.
-              Each inner list represents a row of the triangle, with the
-              values in the row.
-        """
-
         for j in range(1, i):
-            row.append(prev_row[j-1] + prev_row[j])
-
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)
         triangle.append(row)
 
